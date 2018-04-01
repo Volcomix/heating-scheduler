e63432caafa037b451fded0a35c2ab4d18e05585
Volcomix <volcomix@gmail.com>
2018-04-01 16:39:15 +0200

    Draw zones

--- src/app/zone/zone.component.ts
+++ src/app/zone/zone.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
+import * as moment from 'moment';
 
-import { Temperature } from '../temperature.model';
+import { Zone } from '../zone.model';
+
+const minutesInDay = 24 * 60;
 
 @Component({
   selector: 'hs-zone',
@@ -8,7 +11,39 @@ import { Temperature } from '../temperature.model';
   styleUrls: ['./zone.component.css'],
 })
 export class ZoneComponent {
-  @Input() startDate: Date;
-  @Input() endDate: Date;
-  @Input() temperature: Temperature;
+  @Input() zone: Zone;
+
+  @HostBinding('class.first')
+  @Input()
+  isFirst: boolean;
+
+  @HostBinding('class.last')
+  @Input()
+  isLast: boolean;
+
+  @HostBinding('style.background-color')
+  get color() {
+    return this.zone.temperature.color;
+  }
+
+  @HostBinding('style.left.%')
+  get left() {
+    if (this.isFirst) {
+      return 0;
+    } else {
+      const startOfDay = moment(this.zone.startDate).startOf('day');
+      const minutes = -startOfDay.diff(this.zone.startDate, 'minutes');
+      return 100 * minutes / minutesInDay;
+    }
+  }
+  @HostBinding('style.right.%')
+  get right() {
+    if (this.isLast) {
+      return 0;
+    } else {
+      const endOfDay = moment(this.zone.endDate).endOf('day');
+      const minutes = endOfDay.diff(this.zone.endDate, 'minutes');
+      return 100 * minutes / minutesInDay;
+    }
+  }
 }
